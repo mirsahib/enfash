69f972426ef5d5fe376a55d1e15ad01c9b3da4b8
Mir Sahib <mirsahib24@gmail.com>
2022-12-16 19:20:20 +0600

    :bento: Add banner image

--- src/features/Home/components/BannerComponent.tsx
+++ src/features/Home/components/BannerComponent.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const BannerComponent = () => {
     return (
         <View style={{ flex: 2, alignItems: 'center', justifyContent: 'space-evenly' }}>
-            <View style={{ width: "80%", height: "70%", borderRadius: 25, backgroundColor: 'white' }}></View>
+            <View style={{ width: "90%", height: "70%", borderRadius: 25, backgroundColor: '#D1E4A5' }}>
+                <Image source={require('../../../assets/banner_bg.png')} style={{width:'100%',height:'100%'}}/>
+            </View>
             <View style={{ width: "80%", height: "20%", backgroundColor: 'white' }}></View>
         </View>
     )
