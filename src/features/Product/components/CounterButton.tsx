66375388b30481a146ee6b3e5aaff3a3aa64665b
Mir Sahib <mirsahib24@gmail.com>
2022-12-16 19:20:46 +0600

    :hammer: Add rounded button

--- src/features/Product/components/CounterButton.tsx
+++ src/features/Product/components/CounterButton.tsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, ViewStyle } from 'react-native'
 import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/AntDesign';
-import styles,{buttonStyle} from '../styles/component'
+import styles, { buttonStyle } from '../styles/component'
 
 type CounterButtonProps = {
   leftBtnIcon?: string,
@@ -21,32 +21,62 @@ type CounterButtonProps = {
 
 
 const CounterButton = (props: CounterButtonProps) => {
-  const [counter,setCounter] = useState(props.defaultText??0)
+  const [counter, setCounter] = useState(props.defaultText ?? 0)
 
-  const handleLeftBtn = ()=>{
-    if(counter>0){
-      setCounter(counter=>counter-1)
+  const handleLeftBtn = () => {
+    if (counter > 0) {
+      setCounter(counter => counter - 1)
     }
   }
-  const handleRightBtn = ()=>{
-    setCounter(counter=>counter+1)
+  const handleRightBtn = () => {
+    setCounter(counter => counter + 1)
   }
-  
+
   return (
     <View
-      style={[styles.button_container,props.buttonContainerStyle]}>
-      <TouchableOpacity onPress={()=>handleLeftBtn()} style={buttonStyle({borderWidth:props.borderWidth??1,buttonHeight:props.buttonheight??25,buttonWidth:props.buttonWidth??40,buttonColor:props.buttonColor??'#D9D9D9',buttonRadius:props.buttonRadius??10}).btnRight}>
-        <Icon name={props.leftBtnIcon ?? 'minus'} size={props.iconSize ?? 20} color={props.iconColor??"black"} />
+      style={[styles.button_container, props.buttonContainerStyle]}>
+      <TouchableOpacity onPress={() => handleLeftBtn()} style={buttonStyle({ borderWidth: props.borderWidth ?? 1, buttonHeight: props.buttonheight ?? 25, buttonWidth: props.buttonWidth ?? 40, buttonColor: props.buttonColor ?? '#D9D9D9', buttonRadius: props.buttonRadius ?? 10 }).btnRight}>
+        <Icon name={props.leftBtnIcon ?? 'minus'} size={props.iconSize ?? 20} color={props.iconColor ?? "black"} />
       </TouchableOpacity>
-      <View style={buttonStyle({buttonHeight:props.buttonheight??25,buttonWidth:props.buttonWidth??40,buttonColor:props.buttonColor??'#D9D9D9',borderWidth:props.borderWidth??1}).textContainer}>
-        <Text style={{ fontSize: props.textSize ?? 16,color:props.textColor??"black" }}>{counter}</Text>
+      <View style={buttonStyle({ buttonHeight: props.buttonheight ?? 25, buttonWidth: props.buttonWidth ?? 40, buttonColor: props.buttonColor ?? '#D9D9D9', borderWidth: props.borderWidth ?? 1 }).textContainer}>
+        <Text style={{ fontSize: props.textSize ?? 16, color: props.textColor ?? "black" }}>{counter}</Text>
       </View>
-      <TouchableOpacity onPress={()=>handleRightBtn()} style={buttonStyle({borderWidth:props.borderWidth??1,buttonHeight:props.buttonheight??25,buttonWidth:props.buttonWidth??40,buttonColor:props.buttonColor??'#D9D9D9',buttonRadius:props.buttonRadius??10}).btnLeft}>
-        <Icon name={props.leftBtnIcon ?? 'plus'} size={props.iconSize ?? 20} color={props.iconColor??"black"} />
+      <TouchableOpacity onPress={() => handleRightBtn()} style={buttonStyle({ borderWidth: props.borderWidth ?? 1, buttonHeight: props.buttonheight ?? 25, buttonWidth: props.buttonWidth ?? 40, buttonColor: props.buttonColor ?? '#D9D9D9', buttonRadius: props.buttonRadius ?? 10 }).btnLeft}>
+        <Icon name={props.leftBtnIcon ?? 'plus'} size={props.iconSize ?? 20} color={props.iconColor ?? "black"} />
       </TouchableOpacity>
     </View>
   )
 }
 
+
+export const CounterButtonRounded = (props: CounterButtonProps) => {
+  const [counter, setCounter] = useState(props.defaultText ?? 0)
+
+  const handleLeftBtn = () => {
+    if (counter > 0) {
+      setCounter(counter => counter - 1)
+    }
+  }
+  const handleRightBtn = () => {
+    setCounter(counter => counter + 1)
+  }
+
+  return (
+    <View
+      style={[styles.button_container, props.buttonContainerStyle]}>
+      <TouchableOpacity onPress={() => handleLeftBtn()} style={
+        buttonStyle({backgroundColor:props.buttonColor??'white',buttonWidth:props.buttonWidth??20,buttonHeight:props.buttonheight??20,buttonRadius:props.buttonRadius??10}).btnRight
+      } >
+        <Icon name={props.leftBtnIcon ?? 'minus'} size={props.iconSize ?? 20} color={props.iconColor ?? "black"} />
+      </TouchableOpacity>
+      <View style={{ marginHorizontal: 15 }}>
+        <Text style={{ fontSize: props.textSize ?? 16, color: props.textColor ?? "black" }}>{counter}</Text>
+      </View>
+      <TouchableOpacity onPress={() => handleRightBtn()} style={buttonStyle({backgroundColor:props.buttonColor??'black',buttonWidth:props.buttonWidth??20,buttonHeight:props.buttonheight??20,buttonRadius:props.buttonRadius??10}).btnLeft} >
+        <Icon name={props.leftBtnIcon ?? 'plus'} size={props.iconSize ?? 20} color={props.iconColor ?? "white"} />
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 export default CounterButton
