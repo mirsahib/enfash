32a8e4081a9ccb2f60ca07de7f901a390e7b69e7
Mir Sahib <mirsahib24@gmail.com>
2022-12-19 10:47:32 +0600

    :wrench: Add React native path alias

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
+    ["module-resolver",{
+      "alias":{
+        "@assets":"./src/assets",
+        "@components": "./src/components",
+        "@container": "./src/container",
+        "@context": "./src/context",
+        "@features": "./src/features",
+        "@navigation": "./src/navigation",
+        "@store": "./src/store",
+        "@styles": "./src/styles",
+        "@utils": "./src/utils"
+
+      }
+    }]
   ],
   env: {
     production: {
