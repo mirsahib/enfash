f83f8a18fb0a8f38be42268741c098794fbcb111
Mir Sahib <mirsahib24@gmail.com>
2022-12-19 09:56:53 +0600

    :fire: Remove redundant package

--- src/features/Product/container/Product.tsx
+++ src/features/Product/container/Product.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import { ProductNavParams, ProductNavProps } from '..'
 import { useNavigation, useRoute } from '@react-navigation/native'
@@ -21,34 +21,41 @@ const Product = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', }}>
 
-      <Image source={{ uri: params.image }} style={{ flex: 2, width: "100%", height: "90%", marginBottom: '5%' }} />
+      <Image source={{ uri: params.image }} style={{ flex: 1, width: "100%", height: "90%", marginBottom: '5%' }} />
 
-      <View style={{ height: '30%', backgroundColor: 'white', borderTopLeftRadius: 25, borderTopRightRadius: 25, padding: '5%', justifyContent: 'space-between' }}>
-        <View>
-          {/* product name and discription */}
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-            <Text style={{ color: 'black', fontSize: 24, fontWeight: '600', maxWidth: '70%' }} adjustsFontSizeToFit numberOfLines={3}>{params.title}</Text>
-            <View>
-              <Rating
-                type='custom'
-                onFinishRating={(rating: number) => ratingCompleted(rating)}
-                imageSize={20}
-                ratingColor='black'
-                ratingBackgroundColor='#A8A8A8'
-                tintColor='white'
-              />
-              <Text style={{ color: 'black' }}>Rating</Text>
+      <View style={{ flex: 1, backgroundColor: 'white', borderTopLeftRadius: 25, borderTopRightRadius: 25, padding: '5%', justifyContent: 'space-between' }}>
+          <View style={{marginBottom:'2%'}}>
+            {/* product name and discription */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+              <Text style={{ color: 'black', fontSize: 24, fontWeight: '600', maxWidth: '70%' }} adjustsFontSizeToFit numberOfLines={3}>{params.title}</Text>
+              <View>
+                <Rating
+                  type='custom'
+                  onFinishRating={(rating: number) => ratingCompleted(rating)}
+                  imageSize={20}
+                  ratingColor='black'
+                  ratingBackgroundColor='#A8A8A8'
+                  tintColor='white'
+                  showRating
+                  ratingCount={5}
+                  startingValue={params.rating.rate}
+                  style={{}}
+                />
+                <Text style={{ color: 'black' }}>{params.rating.count} reviews</Text>
+              </View>
             </View>
           </View>
-        </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-          {/* button */}
-          <Text style={{ color: 'black', fontSize: 24, fontWeight: '600', flex: 1 }}>$ {params.price}</Text>
-          <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <CounterButton buttonContainerStyle={{ marginRight: '5%' }} buttonWidth={40} buttonheight={38} buttonRadius={15} buttonColor={"white"} />
-            <Button icon={"cart"} mode='contained' onPress={() => handleNavigation()}>Cart</Button>
+          <ScrollView >
+            <Text adjustsFontSizeToFit style={{ color: 'black' }}>{params.description}</Text>
+          </ScrollView>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',marginTop:'2%' }}>
+            {/* button */}
+            <Text style={{ color: 'black', fontSize: 24, fontWeight: '600', flex: 1 }}>$ {params.price}</Text>
+            <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <CounterButton buttonContainerStyle={{ marginRight: '5%' }} buttonWidth={40} buttonheight={38} buttonRadius={15} buttonColor={"white"} />
+              <Button icon={"cart"} mode='contained' onPress={() => handleNavigation()}>Cart</Button>
+            </View>
           </View>
-        </View>
       </View>
     </View>
   )
