326a4eac2a6da065e534a4d52e99dda3b751ceea
Mir Sahib <mirsahib24@gmail.com>
2022-12-20 09:59:54 +0600

    :lipstick: Update theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet } from 'react-native'
+import { Platform, StyleSheet } from 'react-native'
 import React from 'react'
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider ,configureFonts} from 'react-native-paper';
 import AppContainer from './navigation/index'
+
+const fontConfig = {
+  customVariant: {
+    fontFamily: Platform.select({
+      web: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+      ios: 'System',
+      default: 'sans-serif',
+    }),
+    fontWeight: '400',
+    letterSpacing: 0.5,
+    lineHeight: 22,
+    fontSize: 20,
+  }
+};
+
+
 const theme = {
   ...DefaultTheme,
   colors:{
     ...DefaultTheme.colors,
     primary:'black',
     secondary:'#BCC1BA',
-  }
+    background:'#FFFF'
+  },
+  //@ts-ignore
+  fonts:configureFonts({config:fontConfig})
 }
 
 const App = () => {
--- src/features/Home/components/Card.tsx
+++ src/features/Home/components/Card.tsx
@@ -1,49 +1,34 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { useTheme } from 'react-native-paper'
+import { useTheme, Text } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { ProductType } from '../utils/data'
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 
 type CardProps = {
-    data:ProductType
+    data: ProductType
 }
 type MainNavigationParams = {
-    ProductNav:any,
-    HomeNav:any,
+    ProductNav: any,
+    HomeNav: any,
 }
-
 
 const CardComponent = (props: CardProps) => {
     const theme = useTheme()
     const navigation = useNavigation<NativeStackNavigationProp<MainNavigationParams>>()
-    const handleNavigation = ()=>{
-        navigation.navigate('ProductNav',{screen:'Product',params:{...props.data}})        
+    const handleNavigation = () => {
+        navigation.navigate('ProductNav', { screen: 'Product', params: { ...props.data } })
     }
     return (
-        <TouchableOpacity style={{
-            width: 140,
-            paddingBottom:10,
-            backgroundColor: 'white',
-            borderRadius: 15
-        }} onPress={()=>handleNavigation()} >
-            {/* card container */}
-            <View>
-                {/* image container */}
-                <Image source={{ uri: props.data.image }} style={{ width: 140, height: 150 }} />
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: '5%' }}>
-                <View>
-                    {/* text container */}
-                    <Text style={{ color: theme.colors.primary, fontSize: 8, fontWeight: '500' }}>
-                        {props.data.title}
-                    </Text>
-                    <Text style={{ color: theme.colors.primary, fontSize: 12, fontWeight: '500' }}>
-                        $ {props.data.price}
-                    </Text>
-                </View>
-            </View>
-        </TouchableOpacity>
+        <Card onPress={()=>handleNavigation()} style={{width:160,backgroundColor:theme.colors.background}}>
+            <Card.Cover source={{ uri: props.data.image }} style={{resizeMode:'contain'}} />
+            <Card.Content>
+                <Text variant='labelMedium' ellipsizeMode='tail' style={{flex:1}} numberOfLines={2}>{props.data.title}</Text>
+                <Text variant='labelLarge'>$ {props.data.price}</Text>
+            </Card.Content>
+        </Card>
+
     )
 }
 
