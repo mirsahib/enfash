a78c84c130e1b586f9a64b7109fd2f1a467cfeb6
Mir Sahib <mirsahib24@gmail.com>
2022-12-21 18:02:18 +0600

    :package: Add material top bar package

--- src/features/Home/components/ProductComponent.tsx
+++ src/features/Home/components/ProductComponent.tsx
@@ -4,32 +4,9 @@ import Card from './Card'
 import { FlatList } from 'react-native-gesture-handler'
 import data from '../utils/data'
 
-const catData = [
-    {
-        id: "1",
-        category: "shoe",
-    },
-    {
-        id: "2",
-        category: "T-Shirts",
-    }
-]
-
 const ProductComponent = () => {
     return (
         <View style={{ flex: 3 }}>
-            <View style={{paddingHorizontal:'5%',marginVertical:'2%'}}>
-                <FlatList
-                    data={catData}
-                    horizontal={true}
-                    keyExtractor={(item) => item.id}
-                    renderItem={(item) => (
-                        <TouchableOpacity style={{ backgroundColor: 'white',marginHorizontal:5,padding:5,borderRadius:10 }}>
-                            <Text style={{ color: 'black' }}>{item.item.category}</Text>
-                        </TouchableOpacity>
-                    )}
-                />
-            </View>
             <FlatList
                 data={data}
                 numColumns={2}
