9bfc2580cb643c7ae1a027276fcba7444405c341
Mir Sahib <mirsahib24@gmail.com>
2022-12-25 10:36:24 +0600

    :bento: Add fonts and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,9 @@ import store from './store/'
 import { Provider } from 'react-redux';
 
 const fontConfig = {
-  customVariant: {
-    fontFamily: Platform.select({
-      web: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
-      ios: 'System',
-      default: 'sans-serif',
-    }),
-    fontWeight: '400',
-    letterSpacing: 0.5,
-    lineHeight: 22,
-    fontSize: 20,
-  }
+  fontFamily: 'Roboto'
 };
+
 
 
 const theme = {
