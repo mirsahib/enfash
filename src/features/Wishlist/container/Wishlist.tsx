0253dc959eaf53f6d6fbf180452de3031923bd32
Mir Sahib <mirsahib24@gmail.com>
2022-12-28 20:14:45 +0600

    :recycle: Remove redundant code

--- src/features/Wishlist/container/Wishlist.tsx
+++ src/features/Wishlist/container/Wishlist.tsx
@@ -1,15 +1,11 @@
-import { Image, View } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
-import { Button, Card, IconButton, Text, useTheme } from 'react-native-paper'
+import { useTheme } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import IconComponent from '@components/IconComponent'
 import CardComponent from '../components/CardComponent'
 
 const Wishlist = () => {
   const theme = useTheme()
-  const handleNav = () => {
-    console.log('go to product')
-  }
 
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', backgroundColor: theme.colors.primaryContainer }}>
