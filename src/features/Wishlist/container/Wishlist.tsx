b028a494b46e9160f96f96f50fdc80532a845b23
Mir Sahib <mirsahib24@gmail.com>
2022-12-28 20:13:12 +0600

    :sparkles: Add Wishlist

--- src/features/Wishlist/container/Wishlist.tsx
+++ src/features/Wishlist/container/Wishlist.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { Image, View } from 'react-native'
 import React from 'react'
+import { Button, Card, IconButton, Text, useTheme } from 'react-native-paper'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import IconComponent from '@components/IconComponent'
+import CardComponent from '../components/CardComponent'
 
 const Wishlist = () => {
+  const theme = useTheme()
+  const handleNav = () => {
+    console.log('go to product')
+  }
+
   return (
-    <View>
-      <Text>Wishlist</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', backgroundColor: theme.colors.primaryContainer }}>
+      <View style={{ flex: 1, paddingHorizontal: '5%', paddingTop: '5%' }}>
+        <CardComponent/>
+      </View>
+    </SafeAreaView>
   )
 }
 
