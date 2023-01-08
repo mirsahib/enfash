f451d72749f5ef7e13cc6a8a11db3ffbef3464f6
Mir Sahib <mirsahib24@gmail.com>
2023-01-08 11:07:40 +0600

    :recycle: Remove type defination

--- src/features/Product/index.ts
+++ src/features/Product/index.ts
@@ -1,32 +1,13 @@
-import { RouteProp } from "@react-navigation/native";
 import Product from "./container/Product";
 import HeaderRight from "./components/HeaderRight";
 import Cart from "@container/Cart";
-import NavigationOptions from "./components/NavigationOptions";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-type ProductType = {
-    id:string,
-    title:string,
-    price:number,
-    description:string
-    category:string,
-    image:string,
-    rating:{rate:number,count:number}
-}
-
-export type ProductNavParams ={
-    Product:ProductType
-    Cart:any
-}
-
-export type ProductNavProps = RouteProp<ProductNavParams,'Product'>
-export type Props =  NativeStackScreenProps<ProductNavParams>
+import Review from "./container/Review";
 
 const screen = {
     Product:Product,
     HeaderRight:HeaderRight,
     Cart:Cart,
-    NavigationOptions:NavigationOptions
+    Review:Review
 }
 
 export default screen
